aef138ec93fa4893d970f78baaa93e8a8dc4d0bb
Micah Dunson <micahdunson@Micahs-Mac-mini.local>
2017-06-25 14:54:57 -0700

    removed bootstrap js due to non functionality. using vanilla js to achieve sign in register form

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,15 @@
 console.log('App.js here');
+
+const loginForm = $('#log');
+const regForm = $('#reg');
+const signTab = $('.tab')[0];
+const joinTab = $('.tab')[1];
+
+signTab.addEventListener('click', () => {
+  regForm.hide();
+  loginForm.show();
+});
+joinTab.addEventListener('click', () => {
+  regForm.show();
+  loginForm.hide();
+});
