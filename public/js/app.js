87ac85433788eb80e528cd2350d8e002248652c3
Micah Dunson <micahdunson@Micahs-Mac-mini.local>
2017-06-25 15:02:34 -0700

    working on reg form

--- public/js/app.js
+++ public/js/app.js
@@ -5,11 +5,11 @@ const regForm = $('#reg');
 const signTab = $('.tab')[0];
 const joinTab = $('.tab')[1];
 
-signTab.addEventListener('click', () => {
-  regForm.hide();
-  loginForm.show();
-});
-joinTab.addEventListener('click', () => {
-  regForm.show();
-  loginForm.hide();
-});
+// signTab.addEventListener('click', () => {
+//   regForm.hide();
+//   loginForm.show();
+// });
+// joinTab.addEventListener('click', () => {
+//   regForm.show();
+//   loginForm.hide();
+// });
